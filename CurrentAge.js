4b62ed7caa4acc87b1d5263ce606c89b80d0a2db
AnythingGray <89745393+AnythingGray@users.noreply.github.com>
2021-09-17 18:08:04 -0500

    Add files via upload

---
+++ CurrentAge.js
@@ -0,0 +1,11 @@
+var floatAge, floatDays, intWeeks, floatMonths, intFortnights;
+     floatAge = prompt("Enter Your Age in Years");
+     floatDays = parseFloat(floatAge * 365.25);
+     floatMonths = parseFloat(floatAge * 12);
+     intWeeks = parseInt(floatDays / 7);
+     intFortnights = parseInt(floatDays / 14);
+     /*All in one alert bellow */
+     alert("Age in Years = " + floatAge + " Age in Months = " + floatMonths +
+           " Age in Weeks = " + intWeeks + " Age in Fortnights = " + intFortnights +
+           " Age in Days = " + floatDays);
+     
